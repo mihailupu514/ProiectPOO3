10ad123e83c1f3095be7146d889e6d8b6d4c0e73
Mihai Lupu <49549940+mihailupu514@users.noreply.github.com>
2020-01-08 21:44:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,3 @@
-/* Cerințe comune tuturor temelor:
-1. utilizarea sabloanelor
-2. utilizarea STL
-3. utilizarea variabilelor, funcțiilor statice, constantelor și a unei functii const
-4. utilizarea conceptelor de RTTI raportat la template-uri
-5. tratarea excepțiilor
-6. citirea informațiilor complete a n obiecte, memorarea și afișarea acestora
-*/
-/*Tema 6. Se dorește implementarea unei aplicații OOP care sa permita gestionarea activității
-unor farmacii apartinand proprietarului X. Pentru fiecare farmacie se cunoaște cel puțin
-denumirea, numărul de angajați și profiturile pe fiecare luna. Farmaciile pot fi și online. În
-acest caz se cunoaște doar adresa web, numărul de vizitatori și discountul utilizat.
-Structura de date: vector sau list <tuple<web, nr_vizitatori, discount>> se rețin farmaciile
-online
-Cerința suplimentară:
-- Să se adauge toate campurile relevante pentru modelarea acestei probleme.
-- Să se construiască clasa template GestionareFarmacie care sa conțină informații despre
-diversele tipuri de farmacii. Clasa conține indexul farmaciei (incrementat automat la
-adaugarea unei noi file), id-ul lanțului (constant) și o structura de obiecte, alocată dinamic.
-Sa se supraincarce operatorul += pentru inserarea unei noi farmacii online în structura.
-- Să se construiască o specializare pentru tipul Farmacie_online care sa conțină și să afișeze
-doar numărul total de vizitatori ai farmaciilor online.*/
 #include <iostream>
 #include <tuple>
 #include <string>
